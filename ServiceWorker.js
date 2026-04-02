459fd1c957b311faa6b46f83802899cdc89a4d56
Steven Billingslea <sbillingslea@900lbs.com>
2026-04-02 12:08:27 -0500

    updates

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "900lbs of Creative-LA Metro-1.0";
+const contentToCache = [
+    "Build/LAMetro.loader.js",
+    "Build/LAMetro.framework.js",
+    "Build/LAMetro.data",
+    "Build/LAMetro.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
